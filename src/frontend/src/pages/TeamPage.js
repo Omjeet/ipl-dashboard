e700bcbb1109a7d5b1984050b6c92c388a532e5a
Omjeet <omjeetdesi31@gmail.com>
2023-06-21 22:56:13 +0530

    mapping of fetch api to frontend

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,17 +1,42 @@
-import {Reacr} from 'react';
+import {React, useEffect, useState} from 'react';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatcSmallCard';
 
 
 export const TeamPage = () =>   {
+
+  const [team, setTeam] = useState({});
+
+  useEffect(
+
+    () =>{
+      const fetchMatches = async () =>{
+        const response =await fetch('http://localhost:8080/team/Mumbai%20Indians');
+        const data =await response.json();
+        console.log(data);
+        setTeam(data);
+
+      };
+
+      fetchMatches();
+
+    }, []
+
+
+  );
+
+
+
+
+
+
+
   return (
     <div className="TeamPage">
-      <h1>Team Name</h1>
+      <h1>{team.teamName}</h1>
       
       <MatchDetailCard></MatchDetailCard>
-      <MatchSmallCard></MatchSmallCard>
-      <MatchSmallCard></MatchSmallCard>
-      <MatchSmallCard></MatchSmallCard>
+      
     </div>
   );
 }
